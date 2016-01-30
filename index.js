8914cbaf464f8a15d318fa9316a03e0390e35a8c
wednesday_developer <wednesday.developer@gmail.com>
2016-01-30 23:31:24 +0000

    modify

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var QRCode = require('qrcode');
  
 
 http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-type': 'text/html'});
     var msg = req.url;
     QRCode.toDataURL(msg, function(err,url){
+        res.writeHead(200, {'Content-type': 'text/html'});
         res.end('<!DOCTYPE html><html lang="ja"><body><img src="' + url + '"></body></html>');
     });    
 
