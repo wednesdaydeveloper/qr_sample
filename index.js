e9ea5742476a9e4793a5feffa5f89d17b7bec737
wednesday_developer <wednesday.developer@gmail.com>
2016-01-31 03:51:28 +0000

    using socket.io

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-var http = require("http");
-var QRCode = require('qrcode');
- 
 
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-type': 'text/html'});
-    var msg = req.url;
-    QRCode.toDataURL(msg, function(err,url){
-        res.end('<!DOCTYPE html><html lang="ja"><body><img src="' + url + '"></body></html>');
-    });    
+var http = require( 'http' );           //  HTTPモジュール読み込み
+var QRCode = require('qrcode');         //  QRコードモジュール読み込み
+var socketio = require( 'socket.io' );  //  Socket.IOモジュール読み込み
+var fs = require( 'fs' );               //  ファイル入出力モジュール読み込み
 
+// HTTPサーバーを立てる
+var server = http.createServer( function( req, res ) {
+    res.writeHead(200, { 'Content-Type' : 'text/html' }); // ヘッダ出力
+    res.end( fs.readFileSync('./index.html', 'utf-8') );  // index.htmlの内容を出力
 }).listen(process.env.PORT);
 
-console.log("Server running ...");
+// サーバーをソケットに紐付ける
+var io = socketio.listen( server );
+
+// 接続確立後の通信処理部分を定義
+io.sockets.on( 'connection', function( socket ) {
+
+    // QRコードを送り返す
+    socket.on( 'c2s_message', function( data ) {
+        QRCode.toDataURL(data.value, function(err,url){
+            socket.emit( 's2c_message', { qrcode :  url} );
+        });    
+    });
+});
